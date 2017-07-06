a9db6929b2b64ef06acc82c4911c65d10472a49a
ciszkoder <ciszkoder@gmail.com>
2017-07-06 14:24:37 +0200

    Added base class file

---
+++ Main.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace mimori
+{
+    class Mail
+    {
+        List<string> toList = new List<string>();
+        List<string> ccList = new List<string>();
+        List<string> bccList = new List<string>();
+        string subject;
+        string message;
+    }
+    class Main
+    {
+    }
+}
